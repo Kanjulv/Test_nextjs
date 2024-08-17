e5a68889fbb06e4a74f1363fca6aa7cc63f2421b
Kanjulv <118467727+Kanjulv@users.noreply.github.com>
2024-08-17 15:37:03 +0530

    .

--- src/components/Header/DropdownMessage.tsx
+++ src/components/Header/DropdownMessage.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import HelpCenterIcon from '@mui/icons-material/HelpCenter';
 
 const DropdownMessage = () => {
 
@@ -7,7 +6,6 @@ const DropdownMessage = () => {
     <button
       className="bg-gray hover:bg-gray-700 text-black font-bold py-2 px-4 rounded-full flex items-center"
     >
-      <HelpCenterIcon className="mr-2" />
       Help
     </button>
   );
