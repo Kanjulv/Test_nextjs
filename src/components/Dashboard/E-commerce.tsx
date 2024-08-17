8ff62667ab055cdc50590c60084e3b3523fa7885
Kanjulv <118467727+Kanjulv@users.noreply.github.com>
2024-08-17 15:31:56 +0530

    .

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React from "react";
-import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
-import DisplaySettingsOutlinedIcon from '@mui/icons-material/DisplaySettingsOutlined';
 
 const ECommerce: React.FC = () => {
   return (
@@ -42,7 +40,7 @@ const ECommerce: React.FC = () => {
             <hr />
             <div className="mt-4 flex flex-col items-center justify-center py-8">
               <div className="text-center">
-                <span className="block text-3xl font-bold"><ArticleOutlinedIcon/></span>
+                <span className="block text-3xl font-bold"></span>
                 <p className="mt-2 text-lg font-bold">No statements available</p>
                 <p className="mt-1 text-sm text-gray-500">
                   A summary statement will be generated at the end of each month
